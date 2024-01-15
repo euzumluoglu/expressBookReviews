04d062b300fbaf2dfe6143b086d207c13f8e3e48
emin.uzumluoglu <emin.uzumluoglu@adevinta.com>
2024-01-15 17:13:50 +0100

    changes for Hands on Lab: Book Review Application

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,6 +3,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require("axios");
 
 const findByIsin = (ISBN) => {
   return new Promise((resolve, reject) => {
@@ -17,7 +18,7 @@ const findByIsin = (ISBN) => {
 
 const getBooksArray = () => {
   return new Promise((resolve, reject) => {
-    resolve(books);
+    resolve(Object.values(books));
   })
 }
 public_users.post("/register", (req,res) => {
@@ -55,7 +56,8 @@ public_users.get('/author/:author',function (req, res) {
   const author = req.params.author;
   return getBooksArray()
       .then(books => books.filter((book) => book.author === author))
-      .then(filtered_books => res.status(200).json(res.status(200).json(JSON.stringify(filtered_books))));
+      .then(filtered_books => res.status(200).json(res.status(200).json(JSON.stringify(filtered_books))))
+      .catch(err => res.status(404).json(err.message));;
 });
 
 // Get all books based on title
@@ -65,7 +67,7 @@ public_users.get('/title/:title', function (req, res) {
       books => books.filter((book) => book.title === title)
   ).then(
       filtered_books => res.status(200).json(res.status(200).json(JSON.stringify(filtered_books)))
-  );
+  ).catch(err => res.status(404).json(err.message));
 });
 
 //  Get book review
@@ -74,6 +76,48 @@ public_users.get('/review/:isbn',function (req, res) {
   return findByIsin(ISBN)
       .then(book => res.status(200).json(JSON.stringify(book.reviews)))
       .catch(err => res.status(404).json(err));
+});
+
+public_users.get('/server/review/:isbn',function (req, res) {
+  const ISBN = req.params.isbn;
+
+  return findByIsin(ISBN)
+      .then(book => res.status(200).json(JSON.stringify(book.reviews)))
+      .catch(err => res.status(404).json(err));
+});
+
+//to demonstrate fetch all required fetches the server call itself in the code below
+
+//to demonstrate fetch all books the server call itself
+public_users.get('/selfcall/books',async function(req, res) {
+  try {
+    const json = await axios.get('http://localhost:5000/');
+    return res.status(200).json(books);
+  } catch (error) {
+    console.log(`Exception occurred while fetching all books: ${error}`);
+    return res.status(404).json(err)
+  }
+});
+
+public_users.get('/selfcall/isbn/:isbn',function(req, res) {
+  const ISBN = req.params.isbn;
+  return axios.get(`http://localhost:5000/isbn/${ISBN}`)
+      .then(book => res.status(200).json(JSON.stringify(JSON.parse(book.data))))
+      .catch(err => res.status(404).json(err.message));
+});
+
+public_users.get('/selfcall/author/:author',function(req, res) {
+  const author = req.params.author;
+  return axios.get(`http://localhost:5000/author/${author}`)
+      .then(book => res.status(200).json(JSON.stringify(JSON.parse(book.data))))
+      .catch(err => res.status(404).json(err.message));
+});
+
+public_users.get('/selfcall/title/:title',function(req, res) {
+  const title = req.params.title;
+  return axios.get(`http://localhost:5000/title/${title}`)
+      .then(book => res.status(200).json(JSON.stringify(JSON.parse(book.data))))
+      .catch(err => res.status(404).json(err.message));
 });
 
 module.exports.general = public_users;
